bc4163254fd30922336bb7f5c4cbc85ceab25c4b
Zachary Vance <za3k@za3k.com>
2025-11-10 21:01:35 -0500

    Update cookbook

--- generate.sh
+++ generate.sh
@@ -1,37 +1,44 @@
 # Original cookbook contained these characters:
 #  -abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789()/%: .\n+,&[]=?~'*"!@°½⅓¼⅛⅔¾
-#  That is, tr </home/zachary/documents/cookbook-2020.txt -d "\-a-zA-Z0-9()/%: .\n+,&[]=?~'*\"\!@°½⅓¼⅛⅔¾" | wc -c returns 0
+#  That is, tr </home/zachary/documents/cookbook.txt -d "\-a-zA-Z0-9()/%: .\n+,&[]=?~'*\"\!@°½⅓¼⅛⅔¾" | wc -c returns 0
 # Turns out these characters fuck up my printer: ⅛⅔¾
-
 
 # Split the pages
 rm -rf tmp-txt
 mkdir -p tmp-txt
-cat ./cookbook-2020.txt \
+
+# Remove the note at the very end, convert fractions etc
+cat ./cookbook.txt \
   | sed -e "s|1/8|⅛|g" \
   | sed -e "s|1/4|¼|g" \
   | sed -e "s|1/3|⅓|g" \
   | sed -e "s|1/2|½|g" \
   | sed -e "s|2/3|⅔|g" \
   | sed -e "s|'F|°F|g" \
-  | sed -e "s|'C|°C|g" >tmp-cookbook-2020-full.txt
-sed <tmp-cookbook-2020-full.txt -ze "s/===\n/\x00/g" | split - tmp-txt/cookbook- -d --lines 1 -t '\0'
+  | sed -e "s|'C|°C|g" \
+  | head -n-3 \
+  >tmp-cookbook-full.txt
+sed <tmp-cookbook-full.txt -ze "s/===\n/\x00/g" | split - tmp-txt/cookbook- -d --lines 1 -t '\0'
 for x in tmp-txt/*; do
   tr <${x} -d '\000' >/tmp/a
   mv /tmp/a ${x}
 done
 
-# Remove the one page which is too big
-#mv /tmp/cookbook-03 too-big-page
-rm tmp-txt/cookbook-03
-
 # Note: cookbook should be limited to 55 columns
 # Check width
 MAX_LENGTH=$(wc -L tmp-txt/* | tail -n1 | awk '{print $1}')
-[ 55 -ge "$MAX_LENGTH" ] || {
+if [ "$MAX_LENGTH" -gt 55 ]; then 
   echo "Cookbook too wide"
+  for x in tmp-txt/*; do
+    LENGTH=$(wc -L "$x" | tail -n1 | awk '{print $1}')
+    if [ "$LENGTH" -gt 55 ]; then
+      head -n1 "$x"
+    fi
+  done
   exit 2
-}
+fi
+
+# What's the max limit on page height? At least 40, let's say 40?
 
 # Generate .ps files for each txt file
 rm -rf tmp-ps
@@ -52,6 +59,10 @@ find tmp-ps -type f | sort | xargs psjoin >tmp-cookbook-big.ps
 pstops -Pletter -pletter 16:15,0,1,14,13,2,3,12,11,4,5,10,9,6,7,8 <tmp-cookbook-big.ps >tmp-cookbook-shuffled.ps
 psnup -2 -pletter -Pletter -d <tmp-cookbook-shuffled.ps >cookbook.ps
 #rm -rf tmp-*
+
+# Generate PDF files
+pstops tmp-cookbook-big.ps cookbook-big.pdf
+pstops cookbook.ps cookbook-small.pdf
 
 echo
 echo "To print run:"
